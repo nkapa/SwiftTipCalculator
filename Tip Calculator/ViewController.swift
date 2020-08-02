fa30e49a371c90b427b108bfcd7bc37c0173153f
Nandini <nkapa@ucsd.edu>
2020-08-02 16:36:54 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Nandini Kapa on 8/2/20.
+//  Copyright © 2020 Nandini Kapa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
